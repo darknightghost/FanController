5c23cd437ea8e55d4ed871a5d773d96e72fb77ba
Wang Siyuan <darknightghost.cn@gmail.com>
2022-09-17 12:56:54 +0800

    UTC:2022-09-17  4:56:54 :

--- Firmware/source/power/command.c
+++ Firmware/source/power/command.c
@@ -15,18 +15,41 @@
 #include <common/stc8g.h>
 #include <power/command.h>
 
+#define PORT_READY         P3_2
+#define PORT_FAN_POWER     P3_3
+#define PORT_REBOOT_SIGNAL P3_6
+
 /**
  * @brief       Initialize command.
  */
-void command_init() {}
+void command_init()
+{
+    // PORT_READY
+    clear_bit(P3M0, 2);
+    clear_bit(P3M1, 2);
+    set_bit(P3PU.value, 2);
+    PORT_READY = 1;
+
+    // PORT_FAN_POWER
+    clear_bit(P3M0, 3);
+    clear_bit(P3M1, 3);
+    set_bit(P3PU.value, 3);
+    PORT_FAN_POWER = 1;
+
+    // PORT_REBOOT_SIGNAL
+    clear_bit(P3M0, 6);
+    clear_bit(P3M1, 6);
+    set_bit(P3PU.value, 6);
+    PORT_REBOOT_SIGNAL = 1;
+}
 
 /**
  * @brief       Run main loop.
  */
 void command_main_loop()
 {
-    while (true)
-        ;
+    while (true) {
+    }
 }
 
 /**
